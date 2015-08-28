cbe64b3da507b695cf444018f8e30a51cb0f746b
Priya Samuel <psamuel@thoughtworks.com>
2015-08-28 15:01:53 +0100

    Priya: Tty not required

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 docker build -t stubbeego .
 
-docker run -a stdin -a stdout -P -it stubbeego ./test.sh > report.xml
+docker run -a stdin -a stdout -P stubbeego ./test.sh > report.xml
